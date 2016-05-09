3361fb9d421add6d363bbdadc27bd8ba1a53d6d1
jede <jede@oric.org>
2016-05-09 21:37:29 +0200

    adding some vectors

--- telemon.h
+++ telemon.h
@@ -4,20 +4,37 @@
 #define XOP0 $00
 #define XOP1 $01
 
-#define XWSTR0 $14 ; put a str on channel 0
-#define XWSTR1 $15 ; put a str on channel 1
+
+
+#define XRD0 $08
+#define XRD1 $09
+#define XRD2 $0a
+#define XRD3 $0b
+
+
+#define XRDW0 $0c
+#define XRDW1 $0d
+#define XRDW2 $0e
+#define XRDW3 $0f
+
+
 
 
 #define XWR0 $10 ; put a character on channel 0
 #define XWR1 $11 ; put a character on channel 1
-#define XWR1 $12 ; put a character on channel 2
-#define XWR1 $13 ; put a character on channel 3
+#define XWR2 $12 ; put a character on channel 2
+#define XWR3 $13 ; put a character on channel 3
 
+#define XWSTR0 $14 ; put a str on channel 0
+#define XWSTR1 $15 ; put a str on channel 1
+
+#define XDECAL $18 ; COPY mem
 
 #define XTEXT 	$19 ; switch to text
 #define XHIRES 	$1A ; switch to HIRES
 #define XEFFHI 	$1B ; clear HIRES
 
+#define XFILLM $1C ; FILL
 
 #define XCRLF $25 ; send on channel 0, RC and LF (Return and line feed)
 
@@ -32,6 +49,8 @@
 #define XOUPS $42
 #define XSHOOT $47
 
+#define XGOKBD $52 
+#define XMDS $8f ; minitel output
 
 
 
