dc342a0ad8672f324bc30dafe16f7167ce6b11c1
jede <jede@oric.org>
2016-06-03 22:20:44 +0200

    Manage version 2.5 should be done with branch

--- build.bat
+++ build.bat
@@ -1,7 +1,14 @@
+@echo off
 cd src
-..\..\..\osdk\bin\xa.exe -l xa_labels.txt  telemon.asm 
+..\..\..\osdk\bin\xa.exe -C -W -DHAVE_MINITEL=YES  -e error.txt -l xa_labels.txt  telemon.asm 
 
 ..\md5sums a.o65 ..\original\telemon.rom
-cd ..
 
+copy a.o65 ..\release\telemon2_4.rom
+
+..\..\..\osdk\bin\xa.exe -C -W -DHAVE_USBDRIVE=YES  -e error.txt -l xa_labels.txt  telemon.asm -o telemon2_5.rom
+
+copy telemon2_5.rom ..\release\telemon2_5.rom
+copy telemon2_5.rom ..\..\..\oricutron\roms\telemon2_5.rom
+cd ..
  
