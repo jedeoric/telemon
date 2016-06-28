09e36651efec104d7202473f070e07520e43e9fd
jede <jede@oric.org>
2016-06-29 00:43:52 +0200

    Ajout de XWRx.asm

--- build.bat
+++ build.bat
@@ -6,9 +6,9 @@ cd src
 
 copy a.o65 ..\release\telemon2_4.rom
 
-..\..\..\osdk\bin\xa.exe -C -W -DHAVE_USBDRIVE=YES  -e error.txt -l xa_labels.txt  telemon.asm -o telemon2_5.rom
+rem ..\..\..\osdk\bin\xa.exe -C -W -DHAVE_USBDRIVE=YES  -e error.txt -l xa_labels.txt  telemon.asm -o telemon2_5.rom
 
-copy telemon2_5.rom ..\release\telemon2_5.rom
-copy telemon2_5.rom ..\..\..\oricutron\roms\telemon2_5.rom
+rem copy telemon2_5.rom ..\release\telemon2_5.rom
+rem copy telemon2_5.rom ..\..\..\oricutron\roms\telemon2_5.rom
 cd ..
  
---
+++ src/functions/XWRx.asm
@@ -0,0 +1,57 @@
+XWR0_ROUTINE
+	PHA
+	LDA #$00
+	BEQ next37
+XWR1_ROUTINE	
+	PHA
+	LDA #$04
+	BNE next37
+XWR2_ROUTINE
+	PHA
+	LDA #$08
+	BNE next37
+XWR3_ROUTINE	
+	PHA
+	LDA #$0C
+next37
+	STA work_channel
+	PLA
+XWSTR0_re_enter_from_XDECAL	
+	STA i_o_save
+	LDA #$04
+	STA $1A
+	TXA
+	PHA
+	TYA
+	PHA
+Lc77c	
+next39
+	LDX work_channel
+	LDA IOTAB0,X
+	CMP #$88
+	BCC next38
+	ASL
+	TAX
+	LDA ADIOB,X
+	STA $02F8
+	LDA $02BF,X
+	STA $02F9 ; FIXME
+	LDA i_o_save
+	BIT $C795 ; FIXME
+	JSR $02F7 ; FIXME
+Lc79b	
+next38
+	INC work_channel
+	DEC i_o_counter
+	BNE next39
+	PLA
+	TAY
+	PLA
+	TAX
+	LDA $1B
+Lc7a7	
+	RTS
+	
+	
+	
+	
