a29c6c7b15e7adfc7de2689cac59b0bb906c2995
jede <jede@oric.org>
2016-07-02 01:45:26 +0200

    Adding rom for testing

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 @echo off
 cd src
-..\..\..\osdk\bin\xa.exe -C -W -DHAVE_MINITEL=YES  -e error.txt -l xa_labels.txt  telemon.asm 
+..\..\..\..\osdk\bin\xa.exe -C -W -DHAVE_MINITEL=YES  -e error.txt -l xa_labels.txt  telemon.asm 
 
 ..\md5sums a.o65 ..\original\telemon.rom
 
@@ -9,8 +9,8 @@ copy a.o65 ..\release\telemon2_5.rom
 rem ..\..\..\osdk\bin\xa.exe -C -W -DHAVE_USBDRIVE=YES  -e error.txt -l xa_labels.txt  telemon.asm -o telemon2_5.rom
 
 rem copy ..\release\telemon2_5.rom ..\release\telemon2_5.rom
-copy ..\release\telemon2_5.rom ..\..\..\oricutron\roms\telemon2_5.rom
-cd ..\..\..\oricutron\
+copy ..\release\telemon2_5.rom ..\..\..\..\oricutron\roms\telemon2_5.rom
+cd ..\..\..\..\oricutron\
 oricutron -mt -d teledisks\Teledisk05.dsk
 cd ..\projets\telemon
 
