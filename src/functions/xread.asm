56b491a2cbc0a4d12d3998e51965f306bcd790c7
jede <jede@oric.org>
2017-02-04 15:11:29 +0100

    split xread and xwrite code into one file

---
+++ src/functions/xread.asm
@@ -0,0 +1,53 @@
+	
+; [IN] AY contains the length to read
+; [IN] PTR_READ_DEST must be set because it's the ptr_dest
+
+
+.(
+	jsr _ch376_set_bytes_read
+continue	
+	cmp #CH376_USB_INT_DISK_READ; something to read
+	beq we_read
+	cmp #CH376_USB_INT_SUCCESS ; finished
+	beq finished 
+	; TODO  in A : $ff X: $ff
+	lda #0
+	tax
+	rts
+we_read
+	lda #CH376_RD_USB_DATA0
+	sta CH376_COMMAND
+
+	lda CH376_DATA ; contains length read
+	sta TR0; Number of bytes to read
+
+	ldy #0
+loop9
+	lda CH376_DATA ; read the data
+	sta (PTR_READ_DEST),y
+
+	iny
+	cpy TR0
+	bne loop9
+	tya
+	clc
+	adc PTR_READ_DEST
+	bcc next13
+	inc PTR_READ_DEST+1
+next13
+	sta PTR_READ_DEST
+	
+
+	;jmp end_cat
+	lda #CH376_BYTE_RD_GO
+	sta CH376_COMMAND
+	jsr _ch376_wait_response
+	jmp continue
+finished
+	; TODO  return bytes read
+	;lda ptr1
+	;lda #<8000
+	;ldx ptr1+1
+	;ldx #>8000
+	rts	
+.)
---
+++ src/functions/xwrite.asm
@@ -0,0 +1,46 @@
+; [IN] AY contains the length to write
+; [IN] PTR_READ_DEST must be set because it's the ptr_dest
+
+.(	
+    ; save length
+;	sta TR0
+;	sty TR1
+
+	; use ptr1 to count bytes
+	jsr _ch376_set_bytes_write
+	cmp #CH376_USB_INT_SUCCESS ; finished
+	beq start_write
+	rts
+start_write
+    lda     #CH376_CMD_WR_REQ_DATA
+    sta     CH376_COMMAND
+    ldy     CH376_DATA ; contains length read
+    sta     TR0; Number of bytes to read
+
+	ldy #0
+loop:
+    lda     (PTR_READ_DEST),y
+    sta     CH376_DATA ; read the data
+	iny
+    dec     TR0
+    bne     loop
+
+	; compute PTR_READ_DEST
+	sty     TR0
+	lda     PTR_READ_DEST
+	clc
+	adc     TR0
+	bcc     don_t_inc
+	inc     PTR_READ_DEST+1
+don_t_inc	
+	sta     PTR_READ_DEST
+	
+	
+    lda     #CH376_BYTE_WR_GO
+    sta     CH376_COMMAND
+    jsr     _ch376_wait_response
+	cmp     #CH376_USB_INT_SUCCESS ; finished
+    bne     start_write
+	rts	
+	
+.)
