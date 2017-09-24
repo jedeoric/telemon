b6fc852233d1d0fe7112dd7dd3fad30e22a6883a
jede <jede@oric.org>
2017-09-24 10:31:41 +0200

    Cleaning and correcting a bug : in some case (when the length to read is equal to 0), the last 256 bytes of PTR_DEST could be overlaped after the length of the chunk data

--- src/functions/xread.asm
+++ src/functions/xread.asm
@@ -3,51 +3,57 @@
 ; [IN] PTR_READ_DEST must be set because it's the ptr_dest
 ; [IN] TR0 contains the fd id 
 
+; [OUT]  PTR_READ_DEST updated
+; [OUT]  A could contains 0 or the CH376 state
+; [OUT]  Y contains the last size of bytes 
+
+; [UNCHANGED] X
+
 .(
-	jsr _ch376_set_bytes_read
+	jsr     _ch376_set_bytes_read
 continue	
-	cmp #CH376_USB_INT_DISK_READ; something to read
-	beq readme
-	cmp #CH376_USB_INT_SUCCESS ; finished
-	beq finished 
+	cmp     #CH376_USB_INT_DISK_READ  ; something to read
+	beq     readme
+	cmp     #CH376_USB_INT_SUCCESS    ; finished
+	beq     finished 
 	; TODO  in A : $ff X: $ff
-	lda #0
+	lda     #$00
 	tax
 	rts
 readme
-	jsr we_read
+	jsr     we_read
 
-	;jmp end_cat
-	lda #CH376_BYTE_RD_GO
-	sta CH376_COMMAND
-	jsr _ch376_wait_response
-	jmp continue
+	lda     #CH376_BYTE_RD_GO
+	sta     CH376_COMMAND
+	jsr     _ch376_wait_response
+	jmp     continue
 finished
-;	jsr we_read
+  ; at this step PTR_READ_DEST is updated
 	rts	
 
 we_read
-	lda #CH376_RD_USB_DATA0
-	sta CH376_COMMAND
+	lda     #CH376_RD_USB_DATA0
+	sta     CH376_COMMAND
 
-	lda CH376_DATA ; contains length read
-	sta TR0; Number of bytes to read
+	lda     CH376_DATA                ; contains length read
+  beq     finished                  ; we don't have any bytes to read then stops (Assinie report)
+	sta     TR0                       ; Number of bytes to read, storing this value in order to loop
 
-	ldy #0
-loop9
-	lda CH376_DATA ; read the data
-	sta (PTR_READ_DEST),y
-
-	iny
-	cpy TR0
-	bne loop9
-	tya
-	clc
-	adc PTR_READ_DEST
-	bcc next13
-	inc PTR_READ_DEST+1
-next13
-	sta PTR_READ_DEST
+	ldy     #$00
+loop
+	lda     CH376_DATA                ; read the data
+	sta     (PTR_READ_DEST),y         ; send data in the ptr address
+	iny                               ; inc next ptr addrss
+	cpy     TR0                       ; do we read enough bytes
+	bne     loop                      ; no we read
+  
+	tya                               ; We could do "lda TR0" but TYA is quicker. Add X bytes to A in order to update ptr (Y contains the size of the bytes reads)
+	clc                               ; 
+	adc     PTR_READ_DEST
+	bcc     next
+	inc     PTR_READ_DEST+1
+next
+	sta     PTR_READ_DEST
 	rts
 .)
 
