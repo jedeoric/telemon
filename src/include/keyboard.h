5ee384af2091b5e5aec082193f259d94f2277029
jede <jede@oric.org>
2017-10-26 23:13:09 +0200

    add keyboard.h

---
+++ src/include/keyboard.h
@@ -0,0 +1,14 @@
+#define KEY_LCTRL		5
+#define KET_RCTRL		6
+#define KEY_LSHIFT  7
+#define KEY_RSHIFT	8
+#define KEY_FUNCT		9
+
+#define KEY_LEFT		8
+#define KEY_RIGHT		9
+#define KEY_DOWN		10
+#define KEY_UP			11
+
+#define KEY_RETURN $0d
+#define KEY_ESC		 $1b
+#define KEY_DEL		 $7f
